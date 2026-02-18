36177f2037815ca7820dba81cf6b41be36a6bca9
matte-maro <matteomarini2001@gmail.com>
2026-02-18 15:58:11 +0100

    Add news feature with mock data; create news listing and detail pages using Vue components; update styles and structure for improved presentation.

---
+++ app/pages/News/[id].vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <div v-if="article">
+        <NuxtLink to="/news">← Tutte le news</NuxtLink>
+        <header class="py-8">
+          <p>{{ article.date }}</p>
+          <h1>{{ article.title }}</h1>
+        </header>
+  
+        <article>
+          <p>{{ article.excerpt }}</p>
+          <p>{{ article.content }}</p>
+        </article>
+      </div>
+      
+      <div v-else>
+        <h2>News non trovata</h2>
+        <NuxtLink to="/news">Torna all'indice</NuxtLink>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import mockNews from '~/mock/news.json';
+  
+  const route = useRoute();
+  const article = mockNews.find(n => n.id.toString() === route.params.id);
+  </script>
--- app/pages/News/index.vue
+++ app/pages/News/index.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <h1>News</h1>
+    <h1>Ultime News</h1>
+    
+    <ul>
+      <li v-for="article in news" :key="article.id" class="py-8">
+        <NuxtLink :to="`/news/${article.id}`">
+          <span>{{ article.date }}</span>
+          <h2>
+            {{ article.title }}
+          </h2>
+          <p>
+            {{ article.excerpt }}
+          </p>
+          <span>
+            Leggi di più →
+          </span>
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+import mockNews from '~/mock/news.json';
+const news = ref(mockNews);
+</script>
